caa8449c72c07aeb8565830c45f229a13afa5cae
sm2774us <sm2774us@gmail.com>
2020-09-07 13:45:48 -0400

    Adding additonal folders for each sub-topic.
    Adding additonal folders for each sub-topic.

---
+++ Hashing/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import unittest
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().discover('.', pattern = "Solution.py")
+    unittest.TextTestRunner(verbosity=2).run(suite)
